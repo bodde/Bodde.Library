5d464d10f81275ac9b846be8f9cff03d82cca68c
Tomaso Donini <tomaso.donini@gmail.com>
2025-07-26 08:21:27 +0000

    Update Footer component styling; change background class to bg-primary-reverse

--- bodde-library-frontend-react/src/components/Footer.tsx
+++ bodde-library-frontend-react/src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <div className="surface-section p-3 text-center">
+    <div className="p-3 text-center bg-primary-reverse">
       <p className="m-0">© 2025 Bodde Library. All rights reserved.</p>
     </div>
   );
